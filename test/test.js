29d9412390312c86befe393b0a69e82148b117b3
Max Kharandziuk <max.kharandziuk@gmail.com>
2019-07-03 00:53:19 +0200

    add error handling

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ const sharp = require('sharp')
 const { expect } = require('chai')
 const { promisify } = require('util')
 const supertest = require('supertest')
+const domain = require('domain')
 
 const app = express()
 
@@ -12,11 +13,23 @@ const solution = (file, height, width) => {
   return sharp(file).resize({height, width}).toBuffer()
 }
 
-app.post('/resize', (req, res) => {
-  const resizer = sharp().resize({height: 300, width: 300}).jpeg()
+
+app.post('/resize/:height/:width', (req, res) => {
+  const d = domain.create()
+  d.run(() => {
+  const { height, width } = req.params
+  const resizer = sharp().resize({
+    height: Number(height),
+    width: Number(height)
+  }).jpeg()
   req
     .pipe(resizer)
     .pipe(res)
+  })
+  d.on('error', (e) => {
+    console.log(e)
+    res.sendStatus(400)
+  })
 })
 
 describe('service', () => {
@@ -24,7 +37,7 @@ describe('service', () => {
   it('can answer', async () => {
     const image = await promisify(fs.readFile)('test/test-image.jpeg')
     const response = await supertest(app)
-      .post('/resize')
+      .post('/resize/300/300')
       .responseType('blob')
       .set('Content-Type', 'image/jpeg')
       .send(image)
@@ -34,6 +47,17 @@ describe('service', () => {
     const { width, height } = await sharp(response.body).metadata();
     expect(width).equal(300)
     expect(height).equal(300)
+  })
+
+  it('can answer', async () => {
+    const response = await supertest(app)
+      .post('/resize/300/300')
+      .responseType('blob')
+      .set('Content-Type', 'image/jpeg')
+      .send('bbb')
+
+    expect(response.statusCode).equal(400)
+
   })
 
 })
