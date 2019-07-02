71814c597f88fe40a866ffff66a0d00d532c3528
Max Kharandziuk <max.kharandziuk@gmail.com>
2019-07-03 00:10:35 +0200

    add server and a prototype of the solution

--- test/test.js
+++ test/test.js
@@ -1,20 +1,39 @@
 const axios = require('axios')
+const express = require('express')
+const fs = require('fs')
 const sharp = require('sharp')
 const { expect } = require('chai')
-const fs = require('fs')
 const { promisify } = require('util')
+const supertest = require('supertest')
+
+const app = express()
 
 const solution = (file, height, width) => {
   return sharp(file).resize({height, width}).toBuffer()
 }
 
+app.post('/resize', (req, res) => {
+  const resizer = sharp().resize({height: 300, width: 300}).jpeg()
+  req
+    .pipe(resizer)
+    .pipe(res)
+})
 
 describe('service', () => {
-  it('can resize the image', async () => {
+
+  it('can answer', async () => {
     const image = await promisify(fs.readFile)('test/test-image.jpeg')
-    const result = await solution(image, 300, 300)
-    const { width, height } = await sharp(result).metadata();
+    const response = await supertest(app)
+      .post('/resize')
+      .responseType('blob')
+      .set('Content-Type', 'image/jpeg')
+      .send(image)
+
+    expect(response.statusCode).equal(200)
+
+    const { width, height } = await sharp(response.body).metadata();
     expect(width).equal(300)
     expect(height).equal(300)
   })
+
 })
